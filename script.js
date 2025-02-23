e1d88f2c66bbeb5b2db0874b9c64a90f549fb328
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-02-23 20:39:30 -0300

    add interactive submenu functionality and improve layout for better user experience

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButtons = document.querySelectorAll(".menu-button");
+
+    menuButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const targetId = button.getAttribute("data-target");
+            const submenu = document.getElementById(targetId);
+
+            if (submenu) {
+                submenu.classList.toggle("ativo");
+                button.classList.toggle("active");
+            }
+        });
+    });
+});
+
+
