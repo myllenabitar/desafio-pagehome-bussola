4be1f1453704ae7bc40d47370c6b1375b6496a42
Myllena Bitar <111917539+myllenabitar@users.noreply.github.com>
2025-03-03 18:30:19 -0300

    add menu button and toggle functionality for responsive navigation

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
     
+function toggleNav() {
+    var menu = document.getElementById("menu");
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+    } else {
+        menu.classList.add("show");
+    }
+}
+
